09da478fd415fef8cd58697d0968bebb1076a1bb
PingruiLi <lipingrui2001@gmail.com>
2021-09-16 13:31:57 +0800

    Test code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import firebase from "firebase/compat/app";
+//import firebase from "firebase/compat/app";
+import firebase from "firebase/app";
 import "firebase/compat/auth";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import {initializeApp} from "firebase/firebase-app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,11 +16,14 @@ const firebaseConfig = {
   projectId: "ntuacademicforum-f11b9",
   storageBucket: "ntuacademicforum-f11b9.appspot.com",
   messagingSenderId: "927176632912",
-  appId: "1:927176632912:web:673d21fe6ecf3735677f55"
+  appId: "1:927176632912:web:673d21fe6ecf3735677f55",
+  databaseURL: "https://ntuacademicforum-f11b9-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
+let app = initializeApp(firebaseConfig);
+const database = app.databaseURL
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>这是测试</div>
   </div>
 </template>
+
 
 <script>
 // @ is an alias to /src
