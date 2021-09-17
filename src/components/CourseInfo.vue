ddebfe50a19bbca756bb89b860f4fa57fb322227
PingruiLi <lipingrui2001@gmail.com>
2021-09-17 17:26:57 +0800

    add course display page

--- src/components/CourseInfo.vue
+++ src/components/CourseInfo.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <div class="ui huge header">CZ3002</div>
-    <div class="ui segment">
-      <div class="ui top attached label">Course Information</div>
+    <div class="ui black segment">
+      <div class="ui black top attached label" style="font-size: large">Course Information</div>
       <p>CZ3002 is Advanced Software Engineering course </p>
+    </div>
+    <div class="ui black segment">
+      <div class="ui black top attached label" style="font-size: large">Discussion</div>
+      <div class="ui middle aligned divided list">
+        <div class="item">
+          <thread/>
+        </div>
+        <div class="item">
+          <thread/>
+        </div>
+        <div class="item">
+          <thread/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Thread from "@/components/Thread";
 export default {
-name: "CourseInfo"
+name: "CourseInfo",
+  components:{
+    Thread,
+  }
 }
 </script>
 
