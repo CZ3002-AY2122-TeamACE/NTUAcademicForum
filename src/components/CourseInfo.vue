73c1e55571368eb49e295b27dc6e134e83d7ba66
PingruiLi <lipingrui2001@gmail.com>
2021-09-17 18:07:34 +0800

    update thread component

--- src/components/CourseInfo.vue
+++ src/components/CourseInfo.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <div class="ui huge header">CZ3002</div>
+    <div class="ui stackable grid">
+      <div class="nine wide column">
+        <div class="title">CZ3002</div>
+      </div>
+      <div class="six wide column">
+        <div class="horizontal right aligned link list">
+          <button class="ui button">
+            Post Thread
+          </button>
+          <button class="ui button">
+            Swap Index
+          </button>
+          <button class="ui button">
+            Find Teammate
+          </button>
+        </div>
+      </div>
+    </div>
+    <span class="subtitle">1.2k People followed this course</span>
+
     <div class="ui black segment">
       <div class="ui black top attached label" style="font-size: large">Course Information</div>
       <p>CZ3002 is Advanced Software Engineering course </p>
@@ -33,5 +52,12 @@ name: "CourseInfo",
 </script>
 
 <style scoped>
-
+.title{
+  font-size: 40px;
+  font-weight: 900;
+}
+.subtitle{
+  font-size: small;
+  color: darkgrey;
+}
 </style>
