ec47e941f0a4ad023584705aeee38caf6ab1db84
geezzzyyy <zhuoyange@gmail.com>
2021-10-03 16:01:29 +0800

    no message

---
+++ src/components/ThreadReplies.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ui container">
+    <div class="ui black segment">
+      <div class="ui middle aligned divided list">
+        <reply></reply>
+        <reply></reply>
+        <reply></reply>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Reply from "@/components/Reply";
+export default {
+  name: "ThreadReplies",
+  components:{
+    Reply,
+  },
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
