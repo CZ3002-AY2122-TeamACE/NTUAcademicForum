f833c33f3e92a46cebe053c4cd63467b8b9befb4
geezzzyyy <zhuoyange@gmail.com>
2021-10-03 21:43:19 +0800

    login return current user

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,11 @@
   <div>
     <h3>Login</h3>
     <form @submit.prevent="pressed">
+      <div class="alert alert-danger" v-if="this.errors.length > 0">
+        <ul>
+          <li v-for="(error, index) in this.errors" :key="index">{{ error }}</li>
+        </ul>
+      </div>
       <div class="login">
         <input type="text" placeholder="login" v-model="email" />
       </div>
@@ -10,33 +15,108 @@
       </div>
       <button>Login</button>
     </form>
-    <div class="error" v-if="error">{{error.message}}</div>
+<!--    <div v-if="errors.length > 0">-->
+<!--      <div v-for="e in errors"-->
+<!--           :key="e"> e </div>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
-import firebase from "firebase/compat/app";
+// import firebase from "firebase/compat/app";
 import "firebase/auth";
+import main from "../main.js";
 export default {
   data() {
     return {
-      email: "",
-      password: "",
-      error: ""
+      errors: [],
     };
   },
+  computed: {
+    email: {
+      get() {
+        return this.$store.state.auth.email
+      },
+      set(value) {
+        this.$store.commit('setAuthEmail', value)
+      }
+    },
+    password: {
+      get() {
+        return this.$store.state.auth.password
+      },
+      set(value) {
+        this.$store.commit('setAuthPassword', value)
+      }
+    }
+  },
   methods: {
-    pressed() {
-      firebase
-          .auth()
-          .signInWithEmailAndPassword(this.email, this.password)
-          .then(data => {
-            console.log(data);
-            this.$router.replace({ name: "secret" });
-          })
-          .catch(error => {
-            this.error = error;
-          });
+    async pressed() {
+      var self = this;
+      if(this.$store.state.auth.email == "") {
+        this.errors.push('Email address required');
+      }
+
+      if(this.$store.state.auth.password == "") {
+        this.errors.push('Password required');
+      }
+
+      // if(this.$store.state.auth.email !="" && !this.validateEmail(this.$store.state.auth.email)) {
+      //   this.errors.push('Invalid email address');
+      // }
+
+      if(this.$store.state.auth.password !="" && this.$store.state.auth.password.length < 6) {
+        this.errors.push('Password must be at least 6 characters long');
+      }
+
+      if(this.errors.length > 0) {
+
+        return false;
+      }
+
+      // this.isLoading = true;
+
+      // if everthing ok authenticate
+      main.authenticate(this.$store.state.auth.email, this.$store.state.auth.password, function(user) {
+
+        main.getUserByUID(user.user.uid, function(key, val) {
+
+          self.isLoading = false;
+
+          if(key != null && val != null) {
+
+
+            // save these info into store
+            self.$store.commit('setCurrUserId', key);
+            self.$store.commit('setCurrUserName', user.user.displayName);
+            self.$store.commit('setCurrUserEmail', user.user.email);
+            self.$store.commit('setCurrUserUid', user.user.uid);
+            self.$store.commit('setCurrUserStatus', 1);
+
+            // load user forums
+            // main.userForums(user.user.uid, function(response) {
+            //   self.$store.commit('setUserForums', response);
+            // });
+
+            self.$router.push('/');
+          } else {
+            alert("Incomplete user info! try creating account with a different email");
+          }
+        });
+      }, function(error) {
+
+        alert(error.message)
+      })
+      // firebase
+      //     .auth()
+      //     .signInWithEmailAndPassword(this.email, this.password)
+      //     .then(data => {
+      //       console.log(data);
+      //
+      //     })
+      //     .catch(error => {
+      //       this.error = error;
+      //     });
     }
   }
 };
