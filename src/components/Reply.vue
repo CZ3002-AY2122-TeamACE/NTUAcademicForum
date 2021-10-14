5e0e7e7affe12c111a8a8ff83e646b930d16644a
geezzzyyy <zhuoyange@gmail.com>
2021-10-14 16:08:20 +0800

    modify button

--- src/components/Reply.vue
+++ src/components/Reply.vue
@@ -14,12 +14,12 @@
               This will be great for business reports. I will definitely download this.
             </div>
             <div class="actions">
-              <button type="button" class="close" aria-label="Close"><i class="thumbs up outline icon"></i></button>
-              <button type="button" class="close" aria-label="Close"><i class="thumbs down outline icon"></i></button>
-              <button v-b-toggle="'collapse-1'" type="button" class="close" aria-label="Close">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+              <a type="button" ><i class="thumbs up outline icon"></i></a>
+              <a type="button" aria-label="Close"><i class="thumbs down outline icon"></i></a>
+              <a v-b-toggle="'collapse-1'" type="button" aria-label="Close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
                   <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
-                </svg></button>
+                </svg></a>
               <b-collapse id="collapse-1">
                 <!--        <b-card>I am collapsible content!</b-card>-->
                 <post-editor></post-editor>
