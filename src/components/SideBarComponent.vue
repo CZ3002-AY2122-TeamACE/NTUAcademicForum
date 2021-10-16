1e425b0df68bcefdf43f2f78252acda1ff6897f4
WYing333 <e0634980@u.nus.edu>
2021-10-16 19:43:37 +0800

    fix minor bugs

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -15,11 +15,11 @@
             <b-nav vertical>
 
               <b-nav-item active>
-                <b-button block bg-variant="light" v-b-toggle="'collapse-2'">
+                <b-button block bg-variant="light" v-b-toggle="'collapse-1'">
                 <b-icon icon='book'></b-icon>          Subscribed Courses</b-button>
               </b-nav-item>
 
-              <b-collapse id="collapse-2">
+              <b-collapse id="collapse-1">
                 <div class="col text-center" v-for = "(value, key, index) in subCourses" :key="index" >
                   <b-button block @click="hide" variant="dark">
                     <router-link v-bind:to="'/course/'+value.course">{{value.course}}</router-link></b-button>
