acf1d5734dfb7fd8e475f17feea95fdc18aa1c2d
WYing333 <e0634980@u.nus.edu>
2021-10-16 17:43:43 +0800

    add toggle function

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -11,22 +11,22 @@
             <b-avatar badge badge-variant="success" src="https://placekitten.com/300/300" size="4em"></b-avatar>
           </div>
           <nav class="mb-3">
+
             <b-nav vertical>
 
-              <b-nav-item active @click="hide"><b-button bg-variant="light" block @click="hide"><b-icon icon='book'></b-icon>
-                Courses</b-button>
+              <b-nav-item active>
+                <b-button block bg-variant="light" v-b-toggle="'collapse-2'">
+                <b-icon icon='book'></b-icon>          Subscribed Courses</b-button>
               </b-nav-item>
-              <b-button-group vertical>
+
+              <b-collapse id="collapse-2">
                 <div class="col text-center" v-for = "(value, key, index) in subCourses" :key="index" >
                   <b-button block @click="hide" variant="dark">
                     <router-link v-bind:to="'/course/'+value.course">{{value.course}}</router-link></b-button>
                 </div>
-<!--                <div class="button">-->
-<!--                  <router-link :to="`/course/CZ2006`">CZ2006</router-link>-->
-<!--                </div>-->
-<!--                <div class="button">-->
-<!--                  <router-link :to="`/course/CZ3002`">CZ3002</router-link>-->
-<!--                </div>-->
+              </b-collapse>
+
+              <b-button-group vertical>
                 <b-button variant="dark" size = sm>21S1-CZ3002-10358</b-button>
                 <b-button variant="dark" size = sm>21S1-CZ3002-10358</b-button>
               </b-button-group>
@@ -49,7 +49,6 @@
 </template>
 
 <script>
-// import store from "@/Store";
 
 export default {
   name: "SideBarComponent",
